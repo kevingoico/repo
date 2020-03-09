ead2c3095b3338c86e2b0dffca6c23f319c318a9
kevingoico <61964528+kevingoico@users.noreply.github.com>
2020-03-09 09:38:54 +0100

    cambie num3 por num4

--- El mas Grande.py
+++ El mas Grande.py
@@ -1,16 +1,16 @@
 #Programa hecho por kevin goicoechea
 #09/02/2020
 #Es un programa que pide 3 numeros y te devuelve el más grande.
-def grande (num1,num2,num3):
+def grande (num1,num2,num4):
     true = num1
     if true<num2:
         true = num2
-    if true<num3:
-        true = num3
+    if true<num4:
+        true = num4
     print('este es el numero más grande', true)
 
 num1=int(input('pon el primer numero: '))
 num2=int(input('pon el segundo numero: '))
 num3=int(input('pon el ultimo numero: '))
-print('estos son los numeros: ',num1,num2,num3)
-grande(num1,num2,num3)
+print('estos son los numeros: ',num1,num2,num4)
+grande(num1,num2,num4)
